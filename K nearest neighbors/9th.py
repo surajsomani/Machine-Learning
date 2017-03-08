47552a4f20a5259232286f76f55af84792cfaf20
suraj somani <suraj somani>
2017-03-09 01:31:17 +0530

    K nearest Neighbor

---
+++ K nearest neighbors/9th.py
@@ -0,0 +1,30 @@
+import numpy as np
+from sklearn import preprocessing, cross_validation, neighbors
+import pandas as pd
+
+accuracies=[]
+for i in range(25):
+	df = pd.read_csv('breast-cancer-wisconsin.data.txt')
+	df.replace('?',-99999,inplace=True)
+	#df.dropna(inplace=True)
+	#remove the id column
+	df.drop(['id'],1,inplace=True)
+
+	x = np.array(df.drop(['class'],1))
+	y = np.array(df['class'])
+
+	X_train, X_test, Y_train, Y_test = cross_validation.train_test_split(x,y,test_size=0.2)
+
+	clf = neighbors.KNeighborsClassifier()
+	clf.fit(X_train,Y_train)
+
+	accuracy = clf.score(X_test,Y_test)
+	accuracies.append(accuracy)
+	#print(accuracy)
+
+	example_measures = np.array([[4,2,1,1,1,2,3,2,1],[4,2,1,2,2,2,3,2,1]])
+	example_measures = example_measures.reshape(2,-1) #2 sample so (2,-1)
+	#for unknown number we write (len(example_measures),-1)
+	prediction = clf.predict(example_measures)
+	#print(prediction)
+print(sum(accuracies)/len(accuracies))
